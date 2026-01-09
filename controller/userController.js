bb2cdac4c56f462d3f8b774fe3f0252c565da313
instaDev-mayuri <mayuri@instaacoders.com>
2026-01-09 14:48:20 +0530

    Initial commit

---
+++ controller/userController.js
@@ -0,0 +1,79 @@
+import User from "../model/userModel";
+
+// create user
+export const create = async(req, res) =>{
+    try {
+        const newUser = new User(req.body);
+        const {email} = newUser;
+
+        const userExist = await User.findOne({email});
+        if(userExist){
+            return res.status(400).json({ message : 'User already exist!'});
+        }
+        const saveData = await newUser.save();
+        return res.status(200).json(saveData);
+    } catch (error) {
+       res.status(500).json({errMessage : error.message});
+    }
+}
+
+// get user list
+export const getAllUsers = async(req,res) => {
+    try {
+        const userData = await User.find();
+        if(!userData || userData.length === 0){
+            res.status(400).json({ message : 'User not found!'});
+        }
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(500).json({ errMessage : error.message });
+    }
+}
+
+// get user by id
+export const getUserById = async(req, res) =>{
+    try {
+        const id = req.params.id;
+        const userExist = await User.findById(id);
+        if(!userExist) {
+            res.status(400).json({ errMessage : 'User does not exist!'});
+        }
+        res.status(200).json(userExist);
+    } catch (error) {
+        res.status(500).json({ errMessage : error.message});
+    }
+}
+
+// update user
+export const upadte = async (req, res) =>{
+    try {
+        const id = req.params.id;
+        const userExist = await User.findById(id);
+        if(!userExist){
+            res.status(500).json({errMessage : 'User does not exist!'});
+        }
+
+        const updatedData = await User.findOneAndUpdate(id, req.body, {
+            new : true
+        })
+        res.status(200).json(updatedData);
+    } catch (error) {
+        res.status(500).json({ errMessage : error.message});
+    }
+}
+
+// delete user
+export const deleteUser = async (req, res) =>{
+    try {
+        const id =req.params.id;
+        const userExist = await User.findById(id);
+
+        if(!userExist){
+            res.status(500).json({ errMessage : 'User does not exist!'})
+        }
+        await User.findByIdAndDelete(id);
+        res.status(200).json({ message : 'User deleted successfully!'});
+    } catch (error) {
+        res.status(500).json({ errMessage : error.message})
+    }
+}
---
+++ routes/userRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+import { create, deleteUser, getAllUsers, getUserById, upadte } from "../controller/userController.js";
+
+const route =  express.Router();
+
+route.post('/user', create);
+route.get('/users', getAllUsers);
+route.get('/user/:id', getUserById);
+route.put('/user/update/:id', upadte);
+route.delete('/delete/user/:id', deleteUser)
+export default route;
